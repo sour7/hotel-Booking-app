6f30763eef96d5977646b935f1aa193a1e2e7ab7
Sourabh Kumar <95950369+sour7@users.noreply.github.com>
2023-03-15 11:13:40 +0530

    Add files via upload

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./list.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 
 const List = () => {
   return (
-    <div>List</div>
+    <>
+     <Navbar />
+      <Header type="list" />
+    </>
   )
 }
 
