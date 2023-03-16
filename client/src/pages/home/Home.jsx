9aa03e151657d191c03cb791f9f18fba5d62bc4f
Sourabh Kumar <hellosourabh040@gmail.com>
2023-03-16 12:24:40 +0530

    Add files via upload

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
+import MailList from '../../components/mailList/MailList'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
@@ -16,8 +18,8 @@ const Home = () => {
        <PropertyList/>
         <h1 className="homeTitle">Homes guests love</h1>
           <FeaturedProperties/>
-       {/* <MailList/>
-        <Footer/> */}
+      <MailList/>
+         <Footer/>
       </div>
     </div>
   )
