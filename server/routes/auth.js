5c72d3bd7bd40bf14c27b4953d39a59fd81bf009
Sourabh Kumar <hellosourabh040@gmail.com>
2023-03-29 11:19:15 +0530

    auth added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,9 @@
 import express from "express";
-// import { login, register } from "../controllers/auth.js";
+import {login,  register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.send("login from end")
-})
-// router.post("/register", register)
-// router.post("/login", login)
+router.post("/register", register)
+router.post("/login", login)
 
 export default router
